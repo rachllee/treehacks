6216056e67dda4d2c822bb32a0b9560955155702
Rachel Lee <leerache@wharton.upenn.edu>
2024-02-17 15:50:45 -0800

    frontend structure

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AdultInterface from './components/AdultInterface/AdultInterface';
+import ChildInterface from './components/ChildInterface/ChildInterface';
+
+const App = () => {
+  return (
+    <div>
+      <AdultInterface />
+      <ChildInterface />
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/ChildInterface/Bookshelf/Bookshelf.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Bookshelf.css';
+
+const Bookshelf = () => {
+  return (
+    <div className="bookshelf">
+      {/* where books go */}
+    </div>
+  );
+};
+
+export default Bookshelf;
